79db2c32abb4848744936084e9b4ec1580886d38
nasser <nasseraker4@gmail.com>
2024-02-25 20:25:37 +0200

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hii i am nasser");
+        System.out.println("hii i am nasser aker");
 
     }
 }
