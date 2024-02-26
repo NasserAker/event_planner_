405d9f87400a003abae05dfef89b41a496775da9
hala <s12112519@stu.najah.edu>
2024-02-26 23:28:37 +0200

    hala

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
         System.out.println("hii i am hala koni");
         System.out.println("hii i am raya break");
 
-        System.out.println("hii i am hiiii");
+        System.out.println("hii hiiii");
 
     }
 }
